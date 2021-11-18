9da2f9de5c0bb955c42d46f289675abb1c5a22cd
Антон Дмитриевич Пастухов <pastuhov@alma.mater>
2021-11-18 10:16:29 +0300

    changed link --test

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -18,7 +18,7 @@ function Home() {
   return (
     <div className="Home">
         <div className="Photo-main">
-        <img src="https://lh3.googleusercontent.com/ZhGYowXBlPx7ZG8pdj4zfhDshW8QNv1feEIx5bMcw-biPeQMxN09SwfASOF_PNmkUCYGvcdRx0cd9qAXsBgRvFf-R1YMNdOMbFrhRIsW48wQp1xTjLrQqyLFBj376ksADZyRI9BcOjmrytw48_CnWS98SaWDyv1Z6TxL6QswShNEWaK53y4lqDSEiEdxG9puFTxi3N0tlSnZvedt6Pa4EiPDaJYCB3viD00k4fz-u-GdweaSH52OKOyuKNma3WKXxBOG2036nvWlF6OSiowSFAnLhECFFwC0QDlIeqMlDK7rEsboQiJyubdI1cNb_ZbNRk1xDHRlghqJT8O6mJXy3MySgS99qla6F8nI1a7_IyMKWvFc0icwS1mRJeVi9L1PXPylNXcoXih_PrqQcLOHgo9NZ23cNV0W55jF0jIqdSkZ_J8E7wItgOJEHUf3qFphsvvm71FrIa-JMYg4TkAjDRVccw4sZwBaofwXGj04G3RQnZTab16poapcHXRjs9dS2OVa48-qqY6QFO5WO8lI5gFPbAQmNV3fCdloZn2TV2zzIw8TqAG-BMzjgms89_VNn0JxYHJhZYqa_uTDD1qs67zwkTikXQZa2B2AwBJJ_K45G5U0N7-1s4cwDe133ysXIykrmvu0AkCzm53-z3dUV1iGEGBrLWhsQbakO_CTz0lht_hVL1oXxHEacEndqftX17Z09fKhHfyX_mhefRy1ZJg=w1000-h665-no?authuser=0" alt="VLADA"/>
+        <img src="https://photos.app.goo.gl/p4UU9nqodNwWm4Bw6" alt="VLADA"/>
         </div>
         <Menu />
         <AboutMain data={data} />
